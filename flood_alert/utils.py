6b87ab27698e9e9539495a5ef3bbd1130b4f645d
Moritz Geiger <moritzgeiger@hotmail.de>
2021-07-18 15:36:36 +0200

    finalize funcitons, deploy gcf

--- flood_alert/utils.py
+++ flood_alert/utils.py
@@ -1,130 +1,148 @@
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
+import numpy as np
 import smtplib
 import ssl
+import email
+from dotenv import load_dotenv, find_dotenv
+import os
 import datetime as dt
 from time import sleep
 import matplotlib.pyplot as plt
+import plotly.express as px
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email.mime.application import MIMEApplication
 import seaborn as sns
 import base64
-from io import BytesIO
+from io import BytesIO, StringIO
+import tempfile
+from email import encoders
+from os.path import basename
 
 
-def pd_table(base_url, params, alert_levels):
+def table_and_level(base_url, params, checkpoints):
     """
-    returns the biggest visible table on a given url destination.
-    takes in base_url and params (using requests)
+    returns the biggest visible table on a given url destination and the alert level of the tides for several
+    checkpoints in a dictionary {<checkpoint>:{'df':<df>,
+                                                'alert_lvl':<lvl>,
+                                                'level_list':<[list]>}}.
+    takes in base_url (term '___placeholder___' is in the string to insert checkpoint id),
+    params (using requests)
+    AND a dictionary of the checkpoint_id (string) as keys and ja list of threshold alert levels as values.
     """
-    print('pd_table was called.')
-    print(f'fetching table from {base_url}')
-    r = requests.get(base_url, params=params)
+    print('table_and_level() was called.')
+    results = {}
+    for checkpoint, level_list in checkpoints.items():
+        print(f'handling {checkpoint}.')
+        url = base_url.replace('___placeholder___', checkpoint)
+        r = requests.get(url, params=params)
 
-    # get all tables
-    tables = pd.read_html(r.content)
+        # get all tables
+        tables = pd.read_html(r.content)
 
-    # find the biggest table on page
-    lens = [len(x) for x in tables]
-    max_ = lens.index(max(lens))
+        # find the biggest table on page
+        lens = [len(x) for x in tables]
+        max_ = lens.index(max(lens))
 
-    # get table and prepare
-    df = tables[max_]
-    df.columns = ['Datum', 'Wasserstand']
-    df['Datum'] = pd.to_datetime(df.Datum, format='%d.%m.%Y %H:%M')
+        # get table and prepare
+        df = tables[max_]
+        df.columns = ['Datum', f'Wasserstand_{checkpoint}']
+        df['Datum'] = pd.to_datetime(df.Datum, format='%d.%m.%Y %H:%M')
+        df_recent_lvl = df.loc[0, f'Wasserstand_{checkpoint}']
 
-    for k, v in alert_levels.items():
-        df[k] = df.Wasserstand - v
+        # check which level is reached
+        alert_check = [df_recent_lvl > lvl for lvl in level_list]
 
-    return df
+        results[checkpoint] = {'df':df,
+                        'alert_lvl':sum(alert_check),
+                        'level_list':level_list}
+    return results
 
-def alert(df):
-    """
-    Returns the alert level of the reported last timestamp.
-    """
-    print('alert was called.')
-    return (df.set_index('Datum').head(1) > 0).values.sum() - 1
-
-def plot_today_html(df, alert_levels):
+def plot_recent_html(df, lvls):
     """
     Returns the water level plot html encoded filtered to today.
     """
-    print('plot_today_html was called.')
     # filter df
-    today = dt.datetime.today().date()
-    df_today = df[df.Datum.dt.date == today]
+    df_recent = df.head(48)
 
     # set plot
     fig = plt.figure()
     plt.ion() # don't show plot
-    sns.lineplot(data=df_today.set_index('Datum').Wasserstand,
-                 label='Wasserstand')
-    for k,v in alert_levels.items():
-        sns.lineplot(x=df_today.Datum, y=v, label=k)
+    sns.lineplot(data=df_recent.set_index('Datum'))
     plt.xticks(rotation=90)
+    # plot lvls
+    for i,lvl in enumerate(lvls):
+        sns.lineplot(x=df_recent.Datum, y=lvl, label=f'Stufe {i+1}')
     plt.legend()
     plt.grid(axis='y')
+
     plt.close(fig)
 
     # save plot
     tmpfile = BytesIO()
     fig.savefig(tmpfile, format='png')
     encoded = base64.b64encode(tmpfile.getvalue()).decode('utf-8')
-    plot_html = 'Development today:<br><br>' \
-      + '<img src=\'data:image/png;base64,{}\'>'.format(encoded) \
-        + "<br><br>"
+    plot_html = 'Development last 12 hours:<br><br>' + '<img src=\'data:image/png;base64,{}\'>'.format(encoded) + "<br><br>"
 
     return plot_html
 
-def compile_email(homename, sender_email, receiver_email, signature, df, alert_levels):
-    """
-    Compiles email with MIMEMultipart to make it sendable through smtplib (html).
-    Takes these arguments: homename, sender_email, receiver_email, signature, df, alert_levels
-    """
-    print('compilr_email was called.')
-    now = dt.datetime.now().strftime("%y-%m-%d %H:%M:%S")
-    # compile email msg
-    msg = MIMEMultipart('alternative')
-    msg['Subject'] = f"{homename} stats at {now}"
-    msg['From'] = f'Muehlennews <{sender_email}>'
-    msg['To'] = receiver_email
+def send_email(homename, sender_email, receiver_email, password, port, signature, lvl_results, debug):
+    print('send email was called.')
+    # check, if there is any alert in the checkpoints
+    if any([x.get('alert_lvl') for x in lvl_results.values()]) or debug:
+        print('there is an alert!')
+        # compile email msg
+        now = dt.datetime.now().strftime("%y-%m-%d %H:%M:%S")
+        msg = MIMEMultipart('mixed')
+        msg['Subject'] = f"{homename} -WATER ALERT- {now}"
+        msg['From'] = f'{homename} <{sender_email}>'
+        msg['To'] = ','.join(receiver_email)
 
-    # init msg
-    init = "<p>Hi, <br><br>Here are the new water level stats from today.<br><br></p>"
+        # init msg
+        init = "<p>***ALERT***, <br>Es wurde ein ueberhoehter Wasserstand gemeldet.<br><br></p>"
+        # get contents
+        today = dt.datetime.today().date()
 
-    # get contents
-    today = dt.datetime.today().date()
-    table_html = df[df.Datum.dt.date == today].to_html()
-    plot_html = plot_today_html(df, alert_levels)
-    alert_level_html = f"<p>Alert level: {alert(df)}</p>"
+        # unpack dfs
+        plots_html = [plot_recent_html(vals.get('df'), vals.get('level_list')) for id_, vals in lvl_results.items()]
+        alert_levels_html = [f"<p>Alert level {key}: {val.get('alert_lvl')}</p>" for key, val in lvl_results.items()]
 
-    # Record the MIME types of both parts - text/plain and text/html.
-    part1 = MIMEText(init + alert_level_html + plot_html + table_html + signature, 'html')
+        # merge all html per checkpoint together
+        together = '<br><br>'.join([f'{alert_levels_html[x]}\
+                                        <br><br>{plots_html[x]}' for x in range(len(plots_html))])
 
-    # Attach parts into message container.
-    msg.attach(part1)
+        # Record the MIME types of text/html.
+        text = MIMEMultipart('alternative')
+        text.attach(MIMEText(init + together + signature, 'html', _charset="utf-8"))
 
-    return msg
+        # add all parts to msg
+        msg.attach(text)
 
+        # create tables as .csv from temp files
+        tempdir = tempfile.gettempdir()
+        filenames = [f'{tempdir}/{name}.csv' for name in lvl_results.keys()]
+        # filter and save csv tables
+        save_files = [value.get('df').head(48).to_csv(f'{tempdir}/{key}.csv') for key, value in lvl_results.items()]
+        for file_ in filenames:
+            with open(file_, "rb") as fil:
+                part = MIMEApplication(
+                        fil.read(),
+                        Name=basename(file_))
 
-def send_mail(port,
-            password,
-            receiver_email,
-            message,
-            sender_email):
-    """
-    takes in arguments neccessary to send an email.
-    args: port, password, receiver_email, message, sender_email
+            part['Content-Disposition'] = f'attachment; filename={basename(file_)}'
+            msg.attach(part)
 
-    initiates sending of an email.
-    """
-    print('send_mail was called.')
-    # Create a secure SSL context
-    context = ssl.create_default_context()
+        # Create a secure SSL context
+        context = ssl.create_default_context()
 
-    with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
-        server.login(sender_email, password)
-        server.sendmail(sender_email, receiver_email, message)
+        with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
+            server.login(sender_email, password)
+            server.sendmail(sender_email, receiver_email, msg.as_string())
 
-    print(f'mail was sent to {receiver_email}')
+        print(f'successfully sent emails to {receiver_email}')
+    else:
+        _ = [print(f'There was no alert at {id_}') for id_ in lvl_results.keys()]
+        print('Exit function without sending mail.')
